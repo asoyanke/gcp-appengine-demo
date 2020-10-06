82caef8e80e4dd0f7e26cbb53b8660e91eb3aab1
ashok <ashok@example.com>
2020-10-06 17:46:12 +0530

    Rename to AppController

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # Spring Boot API to GCP App Engine
 
 
-Start Locally : mvn spring-boot:run
+Run locally command : mvn spring-boot:run
 
 Swagger UI url : http://localhost:8080/swagger-ui.html
--- src/main/java/com/poc/gcpappenginedemo/controllers/AppController.java
+++ src/main/java/com/poc/gcpappenginedemo/controllers/AppController.java
@@ -1,15 +1,23 @@
 package com.poc.gcpappenginedemo.controllers;
 
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.beans.factory.annotation.Value;
 
 @RestController
-public class HelloController {
+public class AppController {
    @Value("${message}")
-   String message;   
-   @GetMapping(value = "demo")
+   String message;
+   
+   @GetMapping(value="/", produces = MediaType.TEXT_PLAIN_VALUE)
+   public String index() {
+
+       return "This is Home page";
+   }
+   
+   @GetMapping(value = "demo", produces = MediaType.TEXT_PLAIN_VALUE)
    public ResponseEntity<String> displayHelloMessage() {
       return ResponseEntity.ok(message);
    }
